981721b23a845136bde6f5095c268a6d2de95421
Brian Hanquet <brian.hanquet@gmail.com>
2017-07-05 23:18:38 +0200

    optimization + season directory

--- rename_serie.sh
+++ rename_serie.sh
@@ -1,59 +1,56 @@
 #!/bin/bash
 
 # extension des fichiers à garder
-extensions=("avi" "mov" "mpg" "mkv" "mp4" "mp3")
+extensions='.*\.\(avi\|mov\|mpg\|mkv\|mp4\|mp3\)'
 
 # change le séparateur
 IFS=$'\n'
 
 cd "$TR_TORRENT_DIR"
-# Si c'est un dossier, on extrait les bon fichier et on supprimer le dossier
+# Si c'est un dossier, on extrait les bon fichiers et on supprimer le dossier
 if [ -d "$TR_TORRENT_NAME" ]; then
-    for extension in ${extensions[@]}; do
-        # On récupere tous les nom de fichier séparé par un ";"
-        files+=`find "$TR_TORRENT_NAME/" -name "*.$extension"`
-    done
-
-    for file in $files; do
-        if [ -f "$file" ]; then
-            # On supprime seulement le dossier si il y a les bon fichiers dans le dossier
-            deleteDir=true
+    # Récupère tous les fichiers qui ont la bonne extension
+    files=(`find "$TR_TORRENT_NAME/" -iregex $extensions`)
+    if [ $files ]; then
+        for file in ${files[@]}; do
             mv $file .
-        fi
-    done
-    # On récupère le nom du fichier sans le dossier
-    files=${files//"$TR_TORRENT_NAME/"/}
-    if [ $deleteDir ]; then
+        done
         rm -r "$TR_TORRENT_NAME"
     fi
+
+    # On récupère le nom des fichiers sans le dossier
+    files=(${files[@]##*/})
+
 else
     files="$TR_TORRENT_NAME"
 fi
-for fileName in $files; do
+for fileName in ${files[@]}; do
     if [ -f "$fileName" ]; then
         # retire tout ce qui ne sert à rien dans le nom de fichier
-        newFile=${fileName//\[*\]/}
+        newFile=`echo $fileName | sed 's/\.\w*-\w*//ig'`
+        newFile=${newFile//\[*\]}
         newFile=${newFile//\.www/}
-        newFile=${newFile//\.torrent9/}
-        newFile=${newFile//\.cpasbien/}
-        newFile=${newFile//\.ws/}
-        newFile=${newFile//\.biz/}
-        newFile=${newFile//\.cm/}
-        newFile=${newFile//\.pw/}
-        newFile=${newFile//\.io/}
-        newFile=${newFile//\.info/}
-        newFile=${newFile//\ /}
-        mv "$fileName" "$newFile" 2> /dev/null
+        newFile=${newFile//\.torrent9}
+        newFile=${newFile//\.cpasbien}
+        newFile=${newFile//\.ws}
+        newFile=${newFile//\.biz}
+        newFile=${newFile//\.cm}
+        newFile=${newFile//\.pw}
+        newFile=${newFile//\.io}
+        newFile=${newFile//\.info}
+        newFile=${newFile//\ }
+        newFile=${newFile//\\t}
+        newFile=${newFile//\\n}
 
-        # récupere le nom de la série
+        # Permet de récuperer le nom de la série
         firstPart=${newFile%%.*}
         partsName=${newFile#$firstPart.}
         partsName=${partsName//./' '}
-        #On remet le séparateur pour les parties du nom de fichier
+        #On remet le séparateur pour les parties du nom du dossier
         IFS=$' '
         dirName=
         for part in $partsName; do
-            if [[ "$part" =~ ^S[0-9]{1,2}E[0-9]{1,2}|(0?[1-9]|[1-9][0-9]){2}$ ]]; then
+            if [[ "$part" =~ ^S[0-9][1-9]E[0-9][1-9]|(0?[1-9]|[1-9][0-9]){2}$ ]]; then
                 break
             else
                 dirName+="${part^} "
@@ -62,10 +59,20 @@ for fileName in $files; do
         # retire le dernier espace et remet la première partie
         dirName="$firstPart $dirName"
         dirName=${dirName::-1}
+
+        # récupère le numéro de la saison
+        season=`echo $newFile | sed 's/^.*S\([0-9][1-9]\)E[0-9][1-9].*$/\1/i'`
+        season=`echo $season | awk '{firstCar=substr($1,0,1)} {print "Saison",firstCar==0?substr($1,2):$1}'`
+
+        newFile="$dirName/$season/$newFile"
         if ! [ -d "$dirName" ]; then
             mkdir "$dirName"
         fi
-        mv "$newFile" "$dirName"
+
+        if ! [ -d "$dirName/$season" ]; then
+            mkdir "$dirName/$season"
+        fi
+        mv "$fileName" "$newFile" 2> /dev/null
     fi
 done
 
